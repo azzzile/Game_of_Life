c9f07c3501a71ef94e9cc133b3cb9d0344fb6466
Elis Kostromina <liza.kostr.2005@gmail.com>
2022-01-17 04:39:53 +0700

    first commit

---
+++ classes.py
@@ -0,0 +1,80 @@
+import pygame
+
+
+class Field:
+    # сам класс доски. дальше опишу подробно каждый метод
+    def __init__(self, width, height, colors, cell_size):
+        self.field = [[Cell(colors, i, j) for i in range(width)] for j in range(height)]
+        self.width, self.height = width, height  # кол-во клеток на поле
+        self.colors = colors  # colors = (dead cell color, alive cell color)
+        self.cell_size = cell_size  # сторона клетки в пикселях
+
+    def set_boarders(self, boarders):
+        """устанавливаем отступы"""
+        self.boarders = boarders
+
+    def next_step(self, screen, ongoing):
+        """весь процесс (расчет + отрисовка) совершения одного шага в игре. смерть/рождение клеток"""
+        new_field = [[Cell(self.colors, i, j) for i in range(self.width)] for j in range(self.height)]
+
+        for j in range(self.height):
+            for i in range(self.width):
+                cur = self.field[j][i]  # текущая клетка
+                count = self.get_count(i, j)  # кол-во живых соседов клетки
+                pygame.draw.rect(screen, cur.color,
+                                 (self.boarders[0] + i * self.cell_size, self.boarders[1] + j * self.cell_size,
+                                  -self.cell_size, -self.cell_size))  # рисуем саму клетку
+                # определяем ее дальнейшую судьбу
+                if ongoing:
+                    if count in (2, 3) and cur.is_alive() or count == 3 and not cur.is_alive():
+                        new_field[j][i].set_alive(1, self.colors)
+
+                if j == 0:
+                    self.draw_vert(screen, i)  # вертикали сразу же рисуем только на j == 0
+
+            self.draw_hor(screen, j)  # рисуем также и горизонтали
+
+        # нужно нарисовать еще одну вертикаль и горизонталь
+        # (тк их на одну больше, чем ширина и длина поля соответственно)
+        self.draw_vert(screen, self.width)
+        self.draw_hor(screen, self.height)
+
+        self.field = new_field
+        print([x.is_alive() for x in self.field[1]])
+
+    def draw_hor(self, screen, j, grid_color=pygame.Color('white')):
+        """рисуем горизонталь"""
+        pygame.draw.line(screen, grid_color, (self.boarders[0], self.boarders[1] + j * self.cell_size),
+                         (self.boarders[0] + self.width * self.cell_size, self.boarders[1] + j * self.cell_size))
+
+    def draw_vert(self, screen, i, grid_color=pygame.Color('white')):
+        """рисуем вертикаль"""
+        pygame.draw.line(screen, grid_color, (self.boarders[0] + i * self.cell_size, self.boarders[1]),
+                         (self.boarders[0] + i * self.cell_size, self.boarders[1] + self.height * self.cell_size))
+
+    def get_count(self, x, y):
+        """метод, определяющий кол-во живых соседей у клетки xy"""
+        a = sum((self.field[(y + j) % self.height][(x + i) % self.width].is_alive()
+                for i in [-1, 0, 1] for j in [-1, 0, 1])) - self.field[y][x].is_alive()
+        return a
+
+
+class Cell:
+    # класс, реализующий одну клетку
+    def __init__(self, colors, x, y):
+        self.alive = 0
+        self.color = colors[0]
+        self.coors = x, y
+
+    def is_alive(self):
+        """возвращает 1, если клетка живая, 0 - мертвая"""
+        return self.alive
+
+    def set_alive(self, value, colors):
+        """задает параметр"""
+        self.alive = value
+        if value == 0:
+            self.color = colors[0]
+        else:
+            self.color = colors[1]
+
---
+++ main.py
@@ -0,0 +1,105 @@
+import pygame
+import sys
+from classes import Field, Cell
+
+# всякие константы
+BACKGROUND = pygame.Color('darkslategray')
+MAIN_COLOR = pygame.Color('darkseagreen2')
+DEAD_COLOR = pygame.Color('darkseagreen4')
+SIZE = WIDTH, HEIGHT = 620, 670
+FIELD_SIZE = FIELD_WIDTH, FIELD_HEIGHT = 50, 50
+BOARDERS = 30, 90
+CELL_SIZE = min((WIDTH-2*BOARDERS[0])/FIELD_WIDTH, (HEIGHT-BOARDERS[1])/FIELD_HEIGHT)
+FPS = 50
+
+
+def terminate():
+    """выход из игры"""
+    pygame.quit()
+    sys.exit()
+
+
+def load_screen(screen):
+    """на самом деле не нужен, но экран загрузки"""
+    loading_end = pygame.USEREVENT + 1
+    cur_pic = 0
+    cur_let = 0
+    pygame.time.set_timer(loading_end, 5000, 0)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            if event.type == loading_end:
+                return
+        screen.fill(BACKGROUND)
+        line = "LOADING..."[:cur_let]
+
+        string_rendered = FONT.render(line, True, MAIN_COLOR)
+        intro_rect = string_rendered.get_rect()
+        intro_rect.centerx = WIDTH // 2
+        intro_rect.centery = 70
+        screen.blit(string_rendered, intro_rect)
+
+        pygame.display.flip()
+        cur_pic = (cur_pic + 1) % 30
+        cur_let = (cur_let + 1) % 11
+        clock.tick(10)
+
+
+pygame.init()
+FONT = pygame.font.Font('./pix_font.ttf', 45)
+screen = pygame.display.set_mode(SIZE)
+pygame.display.set_caption("Game of Life")
+clock = pygame.time.Clock()
+
+load_screen(screen)  # экран загрузки
+
+field = Field(FIELD_WIDTH, FIELD_HEIGHT, (DEAD_COLOR, MAIN_COLOR), CELL_SIZE)
+field.set_boarders(BOARDERS)
+
+ongoing = False
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        pressed = pygame.mouse.get_pressed()
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            # при нажатии пробела ставим на паузу или продолжаем игру
+            ongoing = not ongoing
+        elif pressed[0] or pressed[2] and ongoing:
+            x, y = pygame.mouse.get_pos()
+            xi, yi = x - BOARDERS[0], y - BOARDERS[1]
+            if xi < 0 and yi < 0:
+                continue
+            i = int(xi // CELL_SIZE)
+            j = int(yi // CELL_SIZE)
+            if i > FIELD_WIDTH or j > FIELD_HEIGHT:
+                continue
+            if pressed[0]:
+                field.field[j][i].set_alive(1, field.colors)
+            elif pressed[2]:
+                field.field[j][i].set_alive(0, field.colors)
+
+    screen.fill(BACKGROUND)
+
+    if ongoing:
+        text = "playing"
+    else:
+        text = "pause"
+
+    string_rendered = FONT.render(text, True, MAIN_COLOR)
+    text_rect = string_rendered.get_rect()
+    text_rect.centerx = WIDTH // 2
+    text_rect.centery = BOARDERS[1] // 2
+    screen.blit(string_rendered, text_rect)
+
+    #print([x.is_alive() for x in field.field[0]])
+
+    field.next_step(screen, ongoing)
+    pygame.display.update()
+    clock.tick(3) # FPS)
+    print(0)
+pygame.quit()
